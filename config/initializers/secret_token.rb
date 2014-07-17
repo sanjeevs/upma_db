5e638ecff565dfe932969f93fc45a8d4d4c442b6
Your name <snjvsingh123@gmail.com>
2014-07-17 18:15:30 -0400

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpmaDb::Application.config.secret_token = 'e7bedd920819a54d817bc93bd4a9a9746a2aafbf6775db22f1d5a002f27071d689ada55ee55980f19665605788203de2e643f9f321bcb50e9739b87073697c18'
