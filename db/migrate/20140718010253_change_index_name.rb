5085ed437034d9cc37e2e731b661cc3438276e7c
Your name <snjvsingh123@gmail.com>
2014-07-20 17:05:38 -0400

    Add precompile assets for Heroku

--- db/migrate/20140718010253_change_index_name.rb
+++
@@ -1,7 +0,0 @@
-class ChangeIndexName < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20140718010410_rename.rb
+++
@@ -1,7 +0,0 @@
-class Rename < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
