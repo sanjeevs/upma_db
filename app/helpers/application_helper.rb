68096f7f6662112a63008f6539fc71fb3631b6a7
Your name <snjvsingh123@gmail.com>
2014-07-27 22:34:48 -0400

    releasing code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,26 @@ module ApplicationHelper
        "#{title}"
     end
   end
+  def to_devanagri_digit(digit)
+    return "\u{0966}" if digit == 0
+    return "\u{0967}" if digit == 1
+    return "\u{0968}" if digit == 2
+    return "\u{0969}" if digit == 3
+    return "\u{096A}" if digit == 4
+    return "\u{096B}" if digit == 5
+    return "\u{096C}" if digit == 6
+    return "\u{096D}" if digit == 7
+    return "\u{096E}" if digit == 8
+    return "\u{096F}" if digit == 9
+  end
+
+  def to_devanagri_number(number)
+    digits = number.to_s.split('').map { |digit| to_devanagri_digit(digit.to_i) }
+    digits.join
+  end
+
+  def to_sanskrit_id(chapter_num, section_num, verse_num)
+    "\u{0965} #{to_devanagri_number(chapter_num)}:#{to_devanagri_number(section_num)}:#{to_devanagri_number(verse_num)} \u{0965}"
+  end
 
 end
