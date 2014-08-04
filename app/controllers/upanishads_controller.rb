6a9c180e7a1b0b39479ace2ddc16ca645e17fdb5
Your name <snjvsingh123@gmail.com>
2014-08-04 19:21:03 -0400

    removed pagination from verses

--- app/controllers/upanishads_controller.rb
+++ app/controllers/upanishads_controller.rb
@@ -2,7 +2,8 @@ class UpanishadsController < ApplicationController
 
   def show
     @upanishad = Upanishad.find(params[:id])
-    @verses = @upanishad.verses.paginate(page: params[:page])
+    #@verses = @upanishad.verses.paginate(page: params[:page])
+    @verses = @upanishad.verses.all
   end
 
   def index
